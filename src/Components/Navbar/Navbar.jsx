f7ee86ef0ae619b24897b069dacf7e131f7ac8f7
mostafa wahba <mostafawahba22710@gmail.com>
2024-04-06 22:55:47 +0200

    css updates

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
-    return <>
-
-    </>
+  return <></>;
 }
