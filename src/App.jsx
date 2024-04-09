d7f93221cc40c0948d2a98e6dc7bf466e13dd2b0
mostafa wahba <mostafawahba22710@gmail.com>
2024-04-10 01:56:47 +0200

    #

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,20 @@ import Register from "./Components/Register/Register";
 import About from "./Components/About/About";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import SingleProduct from "./Components/SingleProduct/SingleProduct";
 const App = () => {
   return (
     <>
-        <Navbar />
-        <Routes>
-          <Route path="" element={<Home />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
-        <Footer />
-
+      <Navbar />
+      <Routes>
+        <Route path="" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/singleProduct" element={<SingleProduct />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
--- src/Components/SingleProduct/SingleProduct.jsx
+++ src/Components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { BsArrowUpSquare } from "react-icons/bs";
-import { BsArrowDownSquare } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa6";
+import { IoIosArrowRoundDown } from "react-icons/io";
+import { IoIosArrowRoundUp } from "react-icons/io";
+
 import { BsHeartFill } from "react-icons/bs";
 import { BiLogoFacebook } from "react-icons/bi";
 import { AiOutlineTwitter } from "react-icons/ai";
@@ -32,210 +34,170 @@ export default function SingleProduct() {
   }
   return (
     <>
-      <div className="container single-product-container d-flex  flex-column justify-content-between align-items-center gap-5">
-        <div className="product-and-product-details d-flex justify-content-center align-items-center gap-5">
-          <div
-            id="carouselExampleIndicators"
-            className="carousel slide w-100"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-indicators">
-              <button
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide-to="0"
-                class="active"
-                aria-current="true"
-                aria-label="Slide 1"
-              >
-                <img src={img} className="d-block w-100" alt="..." />
-              </button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide-to="1"
-                aria-label="Slide 2"
-              >
-                <img src={img1} className="d-block w-100" alt="..." />
-              </button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide-to="2"
-                aria-label="Slide 3"
-              >
-                <img src={img2} className="d-block w-100" alt="..." />
-              </button>
-            </div>
-            <div className="carousel-inner">
-              <div className="carousel-item active w-100">
-                <img src={img} className="d-block w-100" alt="..." />
-              </div>
-              <div className="carousel-item w-100">
-                <img src={img1} className="d-block w-100" alt="..." />
-              </div>
-              <div className="carousel-item w-100">
-                <img src={img2} className="d-block w-100" alt="..." />
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide="prev"
+      <main>
+        <div className="container single-product-container d-flex  flex-column justify-content-between align-items-center gap-5">
+          <div className="product-and-product-details d-flex justify-content-center align-items-center gap-5">
+            <div
+              id="carouselExampleIndicators"
+              className="carousel slide w-100"
+              data-bs-ride="carousel"
             >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next "
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-          <div className="product-details w-100">
-            <h5>Ribbed Bodycon Dress</h5>
-            <div className="price d-flex justify-content-between">
-              <p>$30.00</p>
-              <p className=" text-info curser-pointer">In Stock</p>
-            </div>
-            <hr />
-            <p>
-              Go sporty this summer with this vintage navy and white striped
-              v-neck t-shirt from the Nike. Perfect for pairing with denim and
-              white kicks for a stylish sporty vibe.
-            </p>
-            <div className="count-cart d-flex justify-content-around">
-              <div className="product-count border border-1 d-flex justify-content-between align-items-center gap-5">
-                <span>{count}</span>
-                <div className="counter-btn-container d-flex justify-content-center align-items-center flex-column ">
-                  <button
-                  className=" border-0"
-                  onClick={increment}
-                  style={{
-                    background: "none",
-                  }}
+              <div className="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="0"
+                  class="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
                 >
-                  <div
-                    style={{
-                      backgroundColor: "#ffffff",
-                    }}
-                  >
-                    <BsArrowUpSquare
-                      style={{
-                        color: "#0dcaf0",
-                      }}
-                    />
-                  </div>
+                  <img src={img} className="d-block w-100" alt="..." />
                 </button>
                 <button
-                  className=" border-0"
-                  onClick={decrement}
-                  style={{
-                    background: "none",
-                  }}
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
                 >
-                  <div
-                    style={{
-                      backgroundColor: "#ffffff",
-                    }}
-                  >
-                    <BsArrowDownSquare
-                      style={{
-                        color: "#0dcaf0",
-                      }}
-                    />
-                  </div>
+                  <img src={img1} className="d-block w-100" alt="..." />
                 </button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="2"
+                  aria-label="Slide 3"
+                >
+                  <img src={img2} className="d-block w-100" alt="..." />
+                </button>
+              </div>
+              <div className="carousel-inner">
+                <div className="carousel-item active w-100">
+                  <img src={img} className="d-block w-100" alt="..." />
                 </div>
-                
+                <div className="carousel-item w-100">
+                  <img src={img1} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item w-100">
+                  <img src={img2} className="d-block w-100" alt="..." />
+                </div>
               </div>
-              <button className="btn bg-info p-2 text-white">
-                Add to cart
-              </button>
-              <div
-                style={{
-                  backgroundColor: "#ffffff,",
-                  width: "50px",
-                }}
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev"
               >
-                <BsHeartFill
-                  style={{
-                    color: "#009dff",
-                    cursor: "pointer",
-                  }}
-                />
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next "
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+            <div className="product-details w-100">
+              <h5>Ribbed Bodycon Dress</h5>
+              <div className="price d-flex justify-content-between">
+                <p>$30.00</p>
+                <p className=" text-info curser-pointer">In Stock</p>
               </div>
-            </div>
-            <div className="product-country mt-5">
-              <p>
-                SKU: <span>P-01</span>
+              <hr />
+              <p className="mb-3">
+                Go sporty this summer with this vintage navy and white striped
+                v-neck t-shirt from the Nike. Perfect for pairing with denim and
+                white kicks for a stylish sporty vibe.
               </p>
-              <p>
-                Categories: <span>Accessories, Clothing</span>
-              </p>
-              <p>
-                Tags: <span>Accessories, T-shirt</span>
-              </p>
-            </div>
-            <div className="social-icons d-flex justify-content-center gap-4">
-              <BiLogoFacebook />
-              <AiOutlineTwitter />
-              <SlSocialPintarest />
-              <BsInstagram />
+              <div className="count-cart d-flex justify-content-start align-items-start gap-4">
+                <div className="product-count d-flex justify-content-between align-items-center gap-4 h-100">
+                  <span className="ms-4">{count}</span>
+                  <div className="counter-btn-container d-flex justify-content-center align-items-center flex-column">
+                    <button className="bg-transparent " onClick={increment}>
+                      <IoIosArrowRoundUp />
+                    </button>
+                    <button className="bg-transparent" onClick={decrement}>
+                      <IoIosArrowRoundDown />
+                    </button>
+                  </div>
+                </div>
+                <button className="add-cart-btn text-white transition py-2 px-4 h-100">
+                  Add to cart
+                </button>
+                <div className="heart-icon p-3 h-100">
+                  <FaRegHeart />
+                </div>
+              </div>
+
+              <div className="product-country mt-5">
+                <p>
+                  SKU: <span>P-01</span>
+                </p>
+                <p>
+                  Categories: <span>Accessories, Clothing</span>
+                </p>
+                <p>
+                  Tags: <span>Accessories, T-shirt</span>
+                </p>
+              </div>
+              <div className="social-icons d-flex justify-content-center gap-4">
+                <BiLogoFacebook />
+                <AiOutlineTwitter />
+                <SlSocialPintarest />
+                <BsInstagram />
+              </div>
             </div>
           </div>
+          <section className="related-products">
+            <h4 className=" text-center">Related Product (s)</h4>
+            <div className="products-card d-flex justify-content-between gap-5">
+              <div
+                className=" position-relative card"
+                style={{ width: "18rem;" }}
+              >
+                <img src={img3} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+              {/* <div className="  bg-black position-absolute w-100 h-100 overlaye"></div> */}
+              <div class="card" style={{ width: "18rem;" }}>
+                <img src={img3} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+              <div class="card" style={{ width: "18rem;" }}>
+                <img src={img3} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
-        <div className="related-products">
-          <h4 className=" text-center">Related Product (s)</h4>
-          <div className="products-card d-flex justify-content-between gap-5">
-            <div
-              className=" position-relative card"
-              style={{ width: "18rem;" }}
-            >
-              <img src={img3} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-            {/* <div className="  bg-black position-absolute w-100 h-100 overlaye"></div> */}
-            <div class="card" style={{ width: "18rem;" }}>
-              <img src={img3} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem;" }}>
-              <img src={img3} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </main>
     </>
   );
 }
