531b2172fe2ded620fbec389585b656683bc0b9c
radwaokaily <radwaokaily69@gmail.com>
2024-04-12 22:58:47 +0200

    About us

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,8 +3,13 @@ import './About.css';
 
 export default function About() {
     return <>
-    <div>
-        About
+    <div className="section-background d-flex justify-content-center align-items-center">
+            <h1 className='text-white'>About</h1>
+            </div>
+    <div className='container'>
+        {/* <div className="section-background">
+
+        </div> */}
     </div>
     </>
 }
