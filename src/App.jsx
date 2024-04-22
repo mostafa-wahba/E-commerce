637a7add69992e6c20c88ff758843e2bcd421d38
mostafa wahba <mostafawahba22710@gmail.com>
2024-04-22 22:54:02 +0200

    trying deploy

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     <>
       <Navbar />
       <Routes>
-        <Route path="" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/products" element={<Shop />}></Route>
         <Route path="/login" element={<Login />}></Route>
