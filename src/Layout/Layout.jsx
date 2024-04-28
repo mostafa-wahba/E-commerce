6b290102c424aabc94584e21433a64f07217fe70
mostafa wahba <mostafawahba22710@gmail.com>
2024-04-28 13:50:53 +0300

    fix add to cart & wishlist notifications error

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Layout.css";
 import {Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 export default function Layout() {
   return (
     <>
     <main>
       <Outlet />
+      <ToastContainer />
     </main>
     </>
   );
